6c6e60cf787721b2c1bd251ead601cd1c8cfa081
AleksDemian <demonnvel@gmail.com>
2021-07-12 12:05:27 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:cctracker/CCList.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(CCTracker());
+
+class CCTracker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Awesome CC Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: CCList(),
+    );
+  }
+}
